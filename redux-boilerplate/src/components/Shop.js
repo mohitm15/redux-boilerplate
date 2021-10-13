cb89956d3e3a1c9d31588a1694873fdc97bcb288
Mohit Maroliya <mmaroliya@gmail.com>
2021-10-13 17:05:51 +0530

    Setting up the application

---
+++ redux-boilerplate/src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className="container p-3">
+            <h3>Deposit/Withdraw Money</h3>
+            <button className="btn btn-success mx-2">+</button>
+            You Balance
+            <button className="btn btn-danger mx-2">-</button>
+        </div>
+    )
+}
+
+export default Shop;
